dedb113c7a47fb1fa3730104f4627390b74e2f15
Cheria Artis <Cheria.Artis.Dev@gmail.com>
2021-11-16 11:25:03 -0500

    Doctor class added and WebConfig connection string fixed

---
+++ BPMS.Data/Doctor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BPMS.Data
+{
+    public class Doctor
+    {
+        [Key]
+        public int DoctorId { get; set; }
+
+        [Required]
+        public string DoctorFirstName { get; set; }
+
+        [Required]
+        public string DoctorLastName { get; set; }
+
+        public string DoctorFullName 
+        {
+            get
+            {
+                return DoctorFirstName + " " + DoctorLastName;
+            } 
+        }
+
+        [Required]
+        [MaxLength(50)]
+        public string DoctorSpecialty { get; set; }
+
+        [Required]
+        public bool TakingNewPatients { get; set; }
+    }
+}
